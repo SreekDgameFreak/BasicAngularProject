81b245c087641add07fb91281fa4cc1725f9cdc2
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-08-04 23:46:39 +0530

    Added implementation of how to use @Input @Output and Template reference variables.

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html'
+})
+export class EventThumbnailComponent implements OnInit {
+  
+  //Used to pass information from Parent to child
+  @Input() event: any;
+  //Used to pass information to Parent from Child
+  // @Output() eventClick = new EventEmitter();
+  // clickedMe: number = 0;
+
+  //template reference variable usage
+  // someProperty: any = "some value";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // For sending data to parent component via @Output
+  // handleClickMe() {
+  // this.clickedMe++;
+  //   this.eventClick.emit(this.clickedMe);
+  // }
+
+  // //template reference variable usage
+  // logFoo() {
+  //   console.log('foo');
+  // }
+
+}
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  template: `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <hr>
+      <!-- #thumbnail is the template reference varialbe that can be used to access eventThumbnail Components public properties and methods in events list component -->
+      <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+      <!-- Below is used to get information from the child  -->
+      <!-- <event-thumbnail (eventClick)="handleEventClicked($event)"
+      [event]="event1"></event-thumbnail> -->
+      <!-- {{noOfClicks}} -->
+      
+      <!-- Using template reference variable #thumbnail -->
+      <!-- <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log foo for me</button>
+      <h3>{{thumbnail.someProperty}}</h3> -->
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class EventsListComponent implements OnInit {
+
+  // noOfClicks: number;
+
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2077',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1058 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // handleEventClicked(data) {
+  //   this.noOfClicks=data;
+  // }
+
+}
