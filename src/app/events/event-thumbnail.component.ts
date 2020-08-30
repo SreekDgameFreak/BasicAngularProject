4b8760a54e2f179fcd91a87a2d93421c122e8b5f
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-08-30 20:01:45 +0530

    class binding for single css class

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [`
+    .green { color: #003300 !important; }
     .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 5px;  }
     .well div { color: #bbb; }
