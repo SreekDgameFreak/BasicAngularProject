771731f975d33c0965d8beff65540d4b94b9971e
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-09-05 18:52:08 +0530

    Updated event details to access the parameter being passed and display data based on that.

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-event-details',
@@ -10,10 +11,11 @@ export class EventDetailsComponent implements OnInit {
 
   event: any;
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(
+      +this.route.snapshot.params['id']);
   }
 
 }
