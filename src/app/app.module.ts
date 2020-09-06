2e8c48c5bcc05a2908f5c670b28cfdd65497954c
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-09-06 17:02:08 +0530

    created an error 404 page which can be used to display if a user tries to navigate to an invalid url. Created a route activator service which prevents users from activating/navigating to invalid page. updated path for displaying a particular event id to validate if its an existing id.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavbarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
+import { NavbarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { EventRouteActivatorService } from './events/event-details/event-route-activator.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import{ appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event/create-event.component'
+import { CreateEventComponent } from './events/create-event/create-event.component';
+import { ErrorNotFoundComponent } from './errors/error-not-found.component';
 
 
 @NgModule({
@@ -24,12 +26,14 @@ import { CreateEventComponent } from './events/create-event/create-event.compone
     EventThumbnailComponent,
     NavbarComponent,
     EventDetailsComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    ErrorNotFoundComponent
   ],
   
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivatorService
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router'
 import { EventsListComponent } from './events/events-list.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { CreateEventComponent } from './events/create-event/create-event.component'
+import { ErrorNotFoundComponent } from './errors/error-not-found.component'
+import { EventRouteActivatorService } from './events/event-details/event-route-activator.service'
 
 export const appRoutes:Routes = [
     {
@@ -12,7 +14,11 @@ export const appRoutes:Routes = [
         path : 'events', component: EventsListComponent
     },
     {
-        path : 'events/:id' , component: EventDetailsComponent
+        path : 'events/:id', component: EventDetailsComponent , 
+        canActivate:[EventRouteActivatorService]
+    },
+    {
+        path : '404', component: ErrorNotFoundComponent
     },
     { 
         path: '', redirectTo: '/events', pathMatch: 'full' 
