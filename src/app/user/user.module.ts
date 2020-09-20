ca6d6de156f7ef767738b0fa86183ad06cf189f4
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-09-20 20:53:26 +0530

    Used template based form - login form to demonstrate usage

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user-routes';
+import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ]
 })
