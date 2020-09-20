e9f6d957000f74762fb8cf4bae278bcfa8b89963
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-09-20 22:36:49 +0530

    Added validation in the template based login form.

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
 
   userName: string;
   password: string;
+  mouseoverLogin: boolean;
 
   constructor(private authService: AuthService, private router:Router) { }
 
